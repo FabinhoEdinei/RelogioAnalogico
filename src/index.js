5fc14557ff43715f9fbaed8aac956cc6ffe9cb5c
FabinhoEdinei <59501299+FabinhoEdinei@users.noreply.github.com>
2020-07-04 09:20:30 -0300

    ajustenovo foi trocado o javascript e ajustado o layout do relogio  mas  ficou funcinando melhor.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
-(function () {
-
-  const getElement = el => document.querySelector(el);
-
-  const addRotate = (element, rotacao) => element.style.transform = `rotate(${rotacao}deg)`;
-
-  function relogio() {
-    let date = new Date();
-
-    let s = date.getSeconds() * 6,
-    m = date.getMinutes() * 6 + s / 60,
-    h = date.getHours() * 30 + m / 12;
-
-    addRotate(secEl, s);
-    addRotate(minEl, m);
-    addRotate(houEl, h);
-  }
-
-  const secEl = getElement('#segundos'),
-  minEl = getElement('#minutos'),
-  houEl = getElement('#horas');
-
-  setInterval(relogio, 1000);
-})();
+const sec = document.querySelector(".sec");
+const min = document.querySelector(".min");
+const hr = document.querySelector(".hr");
+setInterval(function(){
+  let time  = new Date();
+  let secs = time.getSeconds() * 6;
+  let mins = time.getMinutes() * 6;
+  let hrs = time.getHours() * 30;
+  sec.style.transform = `rotateZ(${secs}deg)`;
+  min.style.transform = `rotateZ(${mins}deg)`;
+  hr.style.transform = `rotateZ(${hrs+(mins/12)}deg)`;
+});
